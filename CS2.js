2b97af148f715ac5ebe6d6e4ef03478ee3840507
Peter Wielander <peter@minervaproject.com>
2023-09-23 09:10:13 -0700

    Use one JS and CSS file for all case studies

--- CS2.js
+++
@@ -1,5 +0,0 @@
-function ScrollToSection(sectionId) {
-  var section = document.getElementById(sectionId);
-  section.scrollIntoView({ behavior: 'smooth' });
-}
-// These robots are highly adaptable machines <br>that have played an crucial role in a wide range of industries and fields.?
