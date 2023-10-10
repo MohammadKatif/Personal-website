900b37190f49481d2e66146b5842cd1ed4fcff8d
Katif <katifrahim@gmail.com>
2023-10-10 11:06:51 -0700

    Converted horizontal slides into vertical sections

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-// JavaScript function to change slides
-function changeSlide(n) {
-  const myslides = document.getElementsByClassName("slides");
-
-  // Hide all slides
-  for (let i = 0; i < myslides.length; i++) {
-    myslides[i].style.display = "none";
-  }
-
-  // Calculate the next slide index
-  const currentIndex = Array.from(myslides).findIndex((slide) => {
-    return window.getComputedStyle(slide).display !== "none";
-  });
-  let nextIndex = currentIndex + n;
-
-  // Wrap around if reaching the last or first slide
-  if (nextIndex >= myslides.length) {
-    nextIndex = 0;
-  } else if (nextIndex < 0) {
-    nextIndex = myslides.length - 1;
-  }
-
-  // Show the next slide
-  myslides[nextIndex].style.display = "block";
-}
-
-// Show the first slide (sectionHome) initially
-changeSlide(1);
-// gap
+// JS function for navigation bar 
 function ScrollToSection(sectionId) {
   var section = document.getElementById(sectionId);
   section.scrollIntoView({ behavior: 'smooth' });
